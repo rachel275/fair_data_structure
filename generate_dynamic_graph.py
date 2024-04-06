0e7926400d1cb23691b34cadbfa7731867992ac1
rach275 <s1843940@ed.ac.uk>
2024-04-06 10:18:01 +0100

    final graph changes to dynamic graph

--- generate_dynamic_graph.py
+++ generate_dynamic_graph.py
@@ -6,7 +6,7 @@ import csv
 import sys
 
 fig, axs = plt.subplots(3, figsize=(16, 12))
-
+count = 0
 path = "./tables/dynamic/"  #FILL THIS IN HERE
 fileCount = 0;
 for filename in os.listdir(path):
@@ -14,58 +14,70 @@ for filename in os.listdir(path):
     with open(os.path.join(path, filename), 'r') as file:
             type = filename.split('_list')[0]
             print(type)
-            count = -1
             colours = ['r', 'g', 'b', 'k', 'r', 'g', 'b', 'k']
             ratio = [1, 2, 3, 2, 3, 2, 1]
             reader = csv.reader(file)
             i = -1
+            app_thoughput = []
+            temp_app_throughput = []
+            final_app_throughput  = [0]
+            app_total_time = [0]
             for row in reader:
-                app_thoughput = [0]
-                app_total_time = [0]
                 if i == -1:
                     i = 1
                 else:
                     app_thoughput.append(int(row[1]))
-                    app_thoughput.append(int(row[1]))
+                    #app_thoughput.append(int(row[1]))
 
-                    app_total_time.append(int(row[2]))
-                    app_total_time.append(int(row[2]))
+                    app_total_time.append(float(row[2]))
+                    #app_total_time.append(float(row[2]))
 
-                    time_1 = [0, 16, 16, 32, 32, 48, 48, 64, 64, 80, 80, 96, 96, 112, 112, 138, 138]
-                    
-                    print(app_thoughput)
-                    axs[count].plot(time_1, app_thoughput, label = type)
+                    time_1 = [16, 16, 32, 32, 48, 48, 64, 64, 80, 80, 96, 96, 112, 112, 128, 128]
+            print(app_thoughput)
+            for i in range(len(app_thoughput)):
+                 if i == 0:
+                    temp_app_throughput.append(app_thoughput[i])
+                 else:
+                    temp_app_throughput.append(app_thoughput[i] - app_thoughput[i-1] + app_thoughput[0])
+            #print(temp_app_throughput)
+            for i in temp_app_throughput:
+                 final_app_throughput.append(i)
+                 final_app_throughput.append(i)
+            final_app_throughput.append(0)
+            print(final_app_throughput)
+            axs[count].plot(time_1, final_app_throughput, label = type)
 
+            handles, labels = axs[count].get_legend_handles_labels()
 
+            axs[count].text(time_1[0], final_app_throughput[1] + 100000, 'User 1 begins',fontsize=13)
 
-                    handles, labels = axs[count].get_legend_handles_labels()
+            #axs[count].axvline(x = 32, color = 'k', label = 'axvline - full height')
+            axs[count].text(time_1[3], final_app_throughput[3]+ 100000, 'User 2 begins', fontsize=13)
 
+            #axs[count].axvline(x = 64, color = 'k', label = 'axvline - full height')
+            axs[count].text(time_1[5], final_app_throughput[5]+ 100000, 'User 3 begins', fontsize=13)
+            axs[count].text(time_1[7], final_app_throughput[7]+ 100000, 'User 3 ends',fontsize=13)
 
-                    axs[count].text(time_1[0], max(app_thoughput), 'User 1 begins',fontsize=13)
+            #axs[count].axvline(x = 32, color = 'k', label = 'axvline - full height')
+            axs[count].text(time_1[9], final_app_throughput[9]+ 100000, 'User 4 begins', fontsize=13)
 
-                    #axs[count].axvline(x = 32, color = 'k', label = 'axvline - full height')
-                    axs[count].text(time_1[2], max(app_thoughput), 'User 2 begins', fontsize=13)
+            #axs[count].axvline(x = 64, color = 'k', label = 'axvline - full height')
+            axs[count].text(time_1[11], final_app_throughput[11]+ 100000, 'User 4 ends', fontsize=13)
+            axs[count].text(time_1[13], final_app_throughput[13]+ 100000, 'User 2 ends', fontsize=13)   
+            axs[count].text(time_1[15], final_app_throughput[15]+ 100000, 'User 1 ends', fontsize=13)     
+            #by_label = dict(zip(labels, handles))
+            axs[count].set_ylim([1, max(final_app_throughput) + 100000 + 100000])
+            axs[count].set_yticks([1000000, 2000000, 3000000, 4000000])
+            #ylabels = ["1M", "2M", "3M", "4M"]
+            axs[count].set_yticklabels(["1M", "2M", "3M", "4M"])
+            list=["a) Default", "b) Fair", "c) Performance Fair"]
+            axs[count].set_title(list[count]+ " linked list", fontsize=15)
+            #axs[count].legend(by_label.values(), by_label.keys(), loc='upper center', bbox_to_anchor=(1.1, 0.7), title="Insert Ratio", fancybox=True, shadow=True, fontsize=10)
 
-                    #axs[count].axvline(x = 64, color = 'k', label = 'axvline - full height')
-                    axs[count].text(time_1[4], max(app_thoughput), 'User 3 begins', fontsize=13)
-                    axs[count].text(time_1[6], max(app_thoughput), 'User 3 ends',fontsize=13)
-
-                    #axs[count].axvline(x = 32, color = 'k', label = 'axvline - full height')
-                    axs[count].text(time_1[8], max(app_thoughput), 'User 4 begins', fontsize=13)
-
-                    #axs[count].axvline(x = 64, color = 'k', label = 'axvline - full height')
-                    axs[count].text(time_1[10], max(app_thoughput), 'User 4 ends', fontsize=13)
-                    axs[count].text(time_1[12], max(app_thoughput), 'User 2 ends', fontsize=13)   
-                    axs[count].text(time_1[14], max(app_thoughput), 'User 1 ends', fontsize=13)     
-                    by_label = dict(zip(labels, handles))
-
-                    count+= 1
-
-                    axs[1].legend(by_label.values(), by_label.keys(), loc='upper center', bbox_to_anchor=(1.1, 0.7),
-title="Insert Ratio", fancybox=True, shadow=True, fontsize=10)
-#plt.title("Total lock hold per thread")
-axs[1].set_ylabel("Throughput of application", fontsize=10)#,  yscale = "log")#, #ylim=[1, 10**6])
-axs[2].set_xlabel("Time Running", fontsize=10)
+            count += 1
+            print(count)#plt.title("Total lock hold per thread")
+            axs[1].set_ylabel("Throughput of application", fontsize=13)#,  yscale = "log")#, #ylim=[1, 10**6])
+            axs[2].set_xlabel("Time Running", fontsize=13)
 
 plt.tight_layout()
 
--- generate_total_tables.py
+++ generate_total_tables.py
@@ -5,57 +5,55 @@ import os
 import csv
 import sys
 
-final_data = []
 
-rootdir = "./data/"
-for dir in os.listdir(rootdir):
-    type = dir.split('_list')[0]
-    print(type)
-    for filename in os.listdir("./data/" + dir):
-        with open(os.path.join("./data/" + dir, filename), 'r') as file:
-            if filename == "dynamic_applications":
-                applications = 8
-                ratio = [2, 4, 6, 4, 6, 4, 2]
-                throughput = [0, 0, 0, 0, 0, 0, 0]
-                total_cs = [0, 0, 0, 0, 0, 0, 0]
-                duration = 86
-                no_threads = []
-                thread_type = []
-                insert_id = 0
-                find_id = 0
-                tot_time = 0
-                i_tot_time = 0
-                n_ops = 0
-                i_n_ops = 0
-                app_tot_time = 0
-                app_i_tot_time = 0
-                app_n_ops = 0
-                app_i_n_ops = 0
-                for t in range(len(ratio)):
-                    for i in range(ratio[t]):
-                        text_line = file.readline()
-                        # if ((text_line.split(' ')[0] == "Spin_Lock_Opp:") or (not text_line) or (text_line == " ")or (text_line == "\n")):
-                        #     print("ignore")
-                        #     #do nothing with this line
-                        # else:
-                        text = text_line.split('/')
-                        thread_type = text[0].split(' ')[0]
-                        if (thread_type == "id:"):
-                            thread_type = text[0].split(' ')[0]
-                        if (thread_type == 'find'):
-                            thread_id = int(text[0].split(':')[-1]) #int(find_info(text, 1).lstrip().rstrip())
-                            no_ops = int(text[1].split(':')[-1]) #int(find_info(text, 1).lstrip().rstrip())
-                            total_time = float(text[2].split(':')[-1]) #find_info(text, 1).lstrip().rstrip()
-                            throughput[i] += no_ops
-                            total_cs[i] += total_time
-                            #te
-                    
-                final_data.append([type] + throughput + total_cs)
+rootdir = "./data/dynamic"
+
+for filename in os.listdir(rootdir):
+    final_data = []
+    with open(os.path.join(rootdir, filename), 'r') as file:
+        type = filename.split('_linked_list')[0]
+        print(type)
+        applications = 8
+        ratio = [2, 4, 6, 4, 6, 4, 2]
+        throughput = 0 #[0, 0, 0, 0, 0, 0, 0]
+        total_cs = 0#[0, 0, 0, 0, 0, 0, 0]
+        duration = 86
+        no_threads = []
+        thread_type = []
+        insert_id = 0
+        find_id = 0
+        tot_time = 0
+        i_tot_time = 0
+        n_ops = 0
+        i_n_ops = 0
+        app_tot_time = 0
+        app_i_tot_time = 0
+        app_n_ops = 0
+        app_i_n_ops = 0
+        for t in range(len(ratio)):
+            for i in range(ratio[t]):
+                text_line = file.readline()
+                # if ((text_line.split(' ')[0] == "Spin_Lock_Opp:") or (not text_line) or (text_line == " ")or (text_line == "\n")):
+                #     print("ignore")
+                #     #do nothing with this line
+                # else:
+                text = text_line.split('/')
+                thread_type = text[0].split(' ')[0]
+                if (thread_type == "id:"):
+                    thread_type = text[0].split(' ')[0]
+                if (thread_type == 'find'):
+                    thread_id = int(text[0].split(':')[-1]) #int(find_info(text, 1).lstrip().rstrip())
+                    no_ops = int(text[1].split(':')[-1]) #int(find_info(text, 1).lstrip().rstrip())
+                    total_time = float(text[2].split(':')[-1]) #find_info(text, 1).lstrip().rstrip()
+                    throughput += no_ops
+                    total_cs += total_time
+            
+            final_data.append([type, throughput , total_cs])
 
         
-f = open("./tables/default_fair_list_tables/new_dynamic_applications.csv", 'a+', newline="")
-writer = csv.writer(f)
-header = ["Type", "throughput 1", "throughput 1", "throughput 1", "throughput 1", "throughput 1", "throughput 1", "throughput 1", "throughput 1", "time 1", "time 1","time 1","time 1","time 1","time 1","time 1","time 1"]
-writer.writerow(header)
-writer.writerow(final_data)
-f.close()
+        f = open("./tables/dynamic/" + str(type) + "_linked_list.csv", 'a+', newline="")
+        writer = csv.writer(f)
+        header = ["Type", "throughput", "total_cs"]
+        writer.writerow(header)
+        writer.writerow(final_data)
+        f.close()
